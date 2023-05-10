0c049b9c6d6a27fe6da8cf64ed0a4ebb0fb0f849
“Satyam <fouzdarsatyam4@gmail.com>
2023-05-10 14:23:46 +0530

    User Search Implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import signup, login_view
+from accounts.views import signup, login_view, search_users
 
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('search/', search_users, name="user_search")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import authenticate, login
+from django.core.paginator import Paginator, EmptyPage
+from django.db.models import Q
+from django.http import JsonResponse
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from accounts.serializers import CustomUserSerializer
+from accounts.models import CustomUser
 
 
 @api_view(['POST'])
@@ -37,3 +42,41 @@ def login_view(request):
         return Response({'message': 'User logged in successfully.'}, status=status.HTTP_200_OK)
     else:
         return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def search_users(request):
+    """
+    View to search for users using the query parameters of the get request.
+    """
+    query = request.GET.get('q', '')
+
+    if query:
+        users = CustomUser.objects.filter(
+            Q(email__icontains=query) |
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query)
+        ).order_by('id')
+
+        paginator = Paginator(users, 10)
+        page = request.GET.get('page', 1)
+
+        try:
+            users_paginated = paginator.page(page)
+        except EmptyPage:
+            return JsonResponse({'error': 'Invalid page number'}, status=status.HTTP_400_BAD_REQUEST)
+
+        users_list = [
+            {
+                'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+            }
+            for user in users_paginated
+        ]
+
+        return JsonResponse({'users': users_list, 'total_pages': paginator.num_pages})
+
+    return JsonResponse({'error': 'Please provide a valid search query'}, status=status.HTTP_400_BAD_REQUEST)
